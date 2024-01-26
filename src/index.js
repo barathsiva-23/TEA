3dc3728bfc417f42c08558adee427a2e20d6e7af
barath <sit20cs027@sairamtap.edu.in>
2024-01-26 19:31:38 +0530

    pathi over hu

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const pasth=require("path");
+const crypt=require("bcrypt");
+
+const app=express();
+
+app.set('view engine','ejs');
+
+//adding css file here
+app.use(express.static("public"));
+
+app.get('/',(req,res)=>{
+    res.render("login");
+})
+
+app.get('/signup',(req,res)=>{
+    res.render("signup");
+})
+
+
+const port=3000;
+app.listen(port,()=>{
+    console.log(`server is running on port :${port}`);
+})
